ahmed-goda/testimonials

commit d130a2e7ae84b382615bd54dfe54363353c1b542

Testimonials Back-office Import

app/code/Training/Testimonials/etc/adminhtml/menu.xml
@@ -7,7 +7,7 @@
 -->
 <config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:module:Magento_Backend:etc/menu.xsd">
     <menu>
-        <add id="Training_Testimonials::testimonials_menu" title="Testimonials" translate="title" module="Training_Testimonials" sortOrder="200" resource="Training_Testimonials::parent"/>
-        <add id="Training_Testimonials::testimonials" title="Manage Testimonials" translate="title" module="Training_Testimonials" sortOrder="200" parent="Training_Testimonials::testimonials_menu" action="testimonials/testimonial/index" resource="Training_Testimonials::parent"/>
+        <add id="Training_Testimonials::testimonials" title="Testimonials" translate="title" module="Training_Testimonials" sortOrder="200" resource="Training_Testimonials::parent"/>
+        <add id="Training_Testimonials::testimonials_menu" title="Manage Testimonials" translate="title" module="Training_Testimonials" sortOrder="200" parent="Training_Testimonials::testimonials" action="testimonials/testimonial/index" resource="Training_Testimonials::parent"/>
     </menu>
 </config>

app/code/Training/Testimonials/Controller/Adminhtml/Testimonial/Edit.php
@@ -1,11 +1,11 @@
 <?php
 
 namespace Training\Testimonials\Controller\Adminhtml\Testimonial;
+
 use Magento\Backend\App\Action;
 use Magento\Framework\Registry;
 use Magento\Framework\View\Result\PageFactory;
 use Training\Testimonials\Model\Testimonial;
-
 
 class Edit extends Action
 {
@@ -33,28 +33,31 @@ class Edit extends Action
     public function execute()
     {
         $id = $this->getRequest()->getParam('id');
-        $model = $this->testimonial;
+        $testimonial = $this->testimonial;
 
         if($id) {
-            $model->load($id);
-            if(!$model->getId()) {
+            $testimonial->load($id);
+            
+            if(!$testimonial->getId()) {
                 $this->messageManager->addErrorMessage(__('This Testimonial does not exists'));
 
                 $result = $this->resultRedirectFactory->create();
-                return $result->setPath("testimonials/index/index");
+                return $result->setPath("testimonials/testimonial/index");
             }
         }
 
         $data = $this->_getSession()->getFormData(true);
 
         if(!empty($data)) {
-            $model->setData($data);
+            $testimonial->setData($data);
         }
 
-        $this->registry->register('testimonial', $model);
+        $this->registry->register('testimonial', $testimonial);
 
         /** @var \Magento\Backend\Model\View\Result\Page $resultPage */
         $resultPage = $this->pageFactory->create();
+
+        $resultPage->addHandle('testimonials_form');
 
         $resultPage->addBreadcrumb(
             $id ? __('Edit Testimonial') : __('Add a New Testimonial'),

app/code/Training/Testimonials/Controller/Adminhtml/Testimonial/Index.php
@@ -18,12 +18,11 @@ class Index extends Action
        parent::__construct($context);
     }
 
-
     public function execute()
     {
-        return $this->pageFactory->create();
+        //return $this->pageFactory->create();
         $resultPage = $this->pageFactory->create();
-		$resultPage->getConfig()->getTitle()->prepend((__('Posts')));
+		$resultPage->getConfig()->getTitle()->prepend((__('Testimonials')));
 
 		return $resultPage;
     }

app/code/Training/Testimonials/Controller/Adminhtml/Testimonial/NewAction.php
@@ -22,12 +22,6 @@ class NewAction extends Action
         parent::__construct($context);
     }
 
-    protected function _isAllowed()
-    {
-        return $this->_authorization->isAllowed('Training_Testimonials::parent');
-    }
-
-
     public function execute()
     {
         /** @var \Magento\Backend\Model\View\Result\Forward $resultForward */

app/code/Training/Testimonials/Controller/FileUpload.php
@@ -1,0 +1,67 @@
+<?php
+
+namespace Training\Testimonials\Controller;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Filesystem;
+use Magento\MediaStorage\Model\File\UploaderFactory;
+
+class FileUpload extends Action
+{
+    protected $fileSystem;
+
+    protected $uploaderFactory;
+
+    protected $allowedExtensions = ['csv']; // to allow file upload types 
+
+    protected $fileId = 'file_upload'; // name of the input file box  
+
+    public function __construct(
+        Action\Context $context,
+        Filesystem $fileSystem,
+        UploaderFactory $uploaderFactory
+    ) {
+        $this->fileSystem = $fileSystem;
+        $this->uploaderFactory = $uploaderFactory;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $destinationPath = $this->getDestinationPath();
+
+        try {
+            $uploader = $this->uploaderFactory->create(['fileId' => $this->fileId])
+                ->setAllowCreateFolders(true)
+                ->setAllowedExtensions($this->allowedExtensions)
+                ->addValidateCallback('validate', $this, 'validateFile');
+            if (!$uploader->save($destinationPath)) {
+                throw new LocalizedException(
+                    __('File cannot be saved to path: $1', $destinationPath)
+                );
+            }
+
+            // @todo
+            // process the uploaded file
+        } catch (\Exception $e) {
+            $this->messageManager->addError(
+                __($e->getMessage())
+            );
+        }
+    }
+    
+    public function validateFile($filePath)
+    {
+        // @todo
+        // your custom validation code here
+    }
+
+    public function getDestinationPath()
+    {
+        return $this->fileSystem
+            ->getDirectoryWrite(DirectoryList::TMP)
+            ->getAbsolutePath('/');
+    }
+}

app/code/Training/Testimonials/Model/Config/Backend/CustomFileType.php
@@ -1,0 +1,13 @@
+<?php
+ 
+namespace Training\Testimonials\Model\Config\Backend;
+ 
+class CustomFileType extends \Magento\Config\Model\Config\Backend\File
+{
+    /**
+     * @return string[]
+     */
+    public function getAllowedExtensions() {
+        return ['csv', 'xls'];
+    }
+}

app/code/Training/Testimonials/Model/Import/Testimonials.php
@@ -1,0 +1,318 @@
+<?php
+
+namespace Training\Testimonials\Model\Import;
+
+use Exception;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\Json\Helper\Data as JsonHelper;
+use Magento\ImportExport\Helper\Data as ImportHelper;
+use Magento\ImportExport\Model\Import;
+use Magento\ImportExport\Model\Import\Entity\AbstractEntity;
+use Magento\ImportExport\Model\Import\ErrorProcessing\ProcessingErrorAggregatorInterface;
+use Magento\ImportExport\Model\ResourceModel\Helper;
+use Magento\ImportExport\Model\ResourceModel\Import\Data;
+
+/**
+ * Class Testimonials
+ */
+class Testimonials extends AbstractEntity
+{
+    const TABLE = 'testimonials';
+    const ENTITY_ID_COLUMN = 'id';
+    const ENTITY_CODE = 'testimonials';
+
+    /**
+     * If we should check column names
+     */
+    protected $needColumnCheck = true;
+
+    /**
+     * Need to log in import history
+     */
+    protected $logInHistory = true;
+
+    /**
+     * Permanent entity columns.
+     */
+    protected $_permanentAttributes = [
+        'id'
+    ];
+
+    /**
+     * Valid column names
+     */
+    protected $validColumnNames = [
+        'id',
+        'full_name',
+        'testimonial_text'
+    ];
+
+    /**
+     * @var AdapterInterface
+     */
+    protected $connection;
+
+    /**
+     * @var ResourceConnection
+     */
+    private $resource;
+
+    /**
+     * Testimonials constructor.
+     *
+     * @param JsonHelper $jsonHelper
+     * @param ImportHelper $importExportData
+     * @param Data $importData
+     * @param ResourceConnection $resource
+     * @param Helper $resourceHelper
+     * @param ProcessingErrorAggregatorInterface $errorAggregator
+     */
+    public function __construct(
+        JsonHelper $jsonHelper,
+        ImportHelper $importExportData,
+        Data $importData,
+        ResourceConnection $resource,
+        Helper $resourceHelper,
+        ProcessingErrorAggregatorInterface $errorAggregator
+    ) {
+        $this->jsonHelper = $jsonHelper;
+        $this->_importExportData = $importExportData;
+        $this->_resourceHelper = $resourceHelper;
+        $this->_dataSourceModel = $importData;
+        $this->resource = $resource;
+        $this->connection = $resource->getConnection(ResourceConnection::DEFAULT_CONNECTION);
+        $this->errorAggregator = $errorAggregator;
+        $this->initMessageTemplates();
+    }
+
+    /**
+     * Entity type code getter.
+     *
+     * @return string
+     */
+    public function getEntityTypeCode()
+    {
+        return static::ENTITY_CODE;
+    }
+
+    /**
+     * Get available columns
+     *
+     * @return array
+     */
+    public function getValidColumnNames(): array
+    {
+        return $this->validColumnNames;
+    }
+
+    /**
+     * Row validation
+     *
+     * @param array $rowData
+     * @param int $rowNum
+     *
+     * @return boolean
+     */
+    public function validateRow(array $rowData, $rowNum)
+    {
+
+        $name = $rowData['full_name'] ?? '';
+        $duration = $rowData['testimonial_text'] ?? '';
+
+        if (!$name) {
+            $this->addRowError('FullNameIsRequired', $rowNum);
+        }
+
+        if (!$duration) {
+            $this->addRowError('TestimonialTextIsRequired', $rowNum);
+        }
+
+        if (isset($this->_validatedRows[$rowNum])) {
+            return !$this->getErrorAggregator()->isRowInvalid($rowNum);
+        }
+
+        $this->_validatedRows[$rowNum] = true;
+
+        return !$this->getErrorAggregator()->isRowInvalid($rowNum);
+    }
+
+    /**
+     * Import data
+     *
+     * @return bool
+     *
+     * @throws Exception
+     */
+    protected function _importData(): bool
+    {
+        switch ($this->getBehavior()) {
+            case Import::BEHAVIOR_DELETE:
+                $this->deleteEntity();
+                break;
+            case Import::BEHAVIOR_REPLACE:
+                $this->saveAndReplaceEntity();
+                break;
+            case Import::BEHAVIOR_APPEND:
+                $this->saveAndReplaceEntity();
+                break;
+        }
+
+        return true;
+    }
+
+    /**
+     * Delete entities
+     *
+     * @return bool
+     */
+    private function deleteEntity(): bool
+    {
+        $rows = [];
+        while ($bunch = $this->_dataSourceModel->getNextBunch()) {
+            foreach ($bunch as $rowNum => $rowData) {
+                $this->validateRow($rowData, $rowNum);
+
+                if (!$this->getErrorAggregator()->isRowInvalid($rowNum)) {
+                    $rowId = $rowData[static::ENTITY_ID_COLUMN];
+                    $rows[] = $rowId;
+                }
+
+                if ($this->getErrorAggregator()->hasToBeTerminated()) {
+                    $this->getErrorAggregator()->addRowToSkip($rowNum);
+                }
+            }
+        }
+
+        if ($rows) {
+            return $this->deleteEntityFinish(array_unique($rows));
+        }
+
+        return false;
+    }
+
+    /**
+     * Save and replace entities
+     *
+     * @return void
+     */
+    private function saveAndReplaceEntity()
+    {
+        $behavior = $this->getBehavior();
+        $rows = [];
+        while ($bunch = $this->_dataSourceModel->getNextBunch()) {
+            $entityList = [];
+
+            foreach ($bunch as $rowNum => $row) {
+                if (!$this->validateRow($row, $rowNum)) {
+                    continue;
+                }
+
+                if ($this->getErrorAggregator()->hasToBeTerminated()) {
+                    $this->getErrorAggregator()->addRowToSkip($rowNum);
+
+                    continue;
+                }
+
+                $rowId = $row[static::ENTITY_ID_COLUMN];
+                $rows[] = $rowId;
+                $columnValues = [];
+
+                foreach ($this->getAvailableColumns() as $columnKey) {
+                    $columnValues[$columnKey] = $row[$columnKey];
+                }
+
+                $entityList[$rowId][] = $columnValues;
+                $this->countItemsCreated += (int) !isset($row[static::ENTITY_ID_COLUMN]);
+                $this->countItemsUpdated += (int) isset($row[static::ENTITY_ID_COLUMN]);
+            }
+
+            if (Import::BEHAVIOR_REPLACE === $behavior) {
+                if ($rows && $this->deleteEntityFinish(array_unique($rows))) {
+                    $this->saveEntityFinish($entityList);
+                }
+            } elseif (Import::BEHAVIOR_APPEND === $behavior) {
+                $this->saveEntityFinish($entityList);
+            }
+        }
+    }
+
+    /**
+     * Save entities
+     *
+     * @param array $entityData
+     *
+     * @return bool
+     */
+    private function saveEntityFinish(array $entityData): bool
+    {
+        if ($entityData) {
+            $tableName = $this->connection->getTableName(static::TABLE);
+            $rows = [];
+
+            foreach ($entityData as $entityRows) {
+                foreach ($entityRows as $row) {
+                    $rows[] = $row;
+                }
+            }
+
+            if ($rows) {
+                $this->connection->insertOnDuplicate($tableName, $rows, $this->getAvailableColumns());
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    /**
+     * Delete entities
+     *
+     * @param array $entityIds
+     *
+     * @return bool
+     */
+    private function deleteEntityFinish(array $entityIds): bool
+    {
+        if ($entityIds) {
+            try {
+                $this->countItemsDeleted += $this->connection->delete(
+                    $this->connection->getTableName(static::TABLE),
+                    $this->connection->quoteInto(static::ENTITY_ID_COLUMN . ' IN (?)', $entityIds)
+                );
+
+                return true;
+            } catch (Exception $e) {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Get available columns
+     *
+     * @return array
+     */
+    private function getAvailableColumns(): array
+    {
+        return $this->validColumnNames;
+    }
+
+    /**
+     * Init Error Messages
+     */
+    private function initMessageTemplates()
+    {
+        $this->addMessageTemplate(
+            'FullNameIsRequired',
+            __('The full name cannot be blank.')
+        );
+        $this->addMessageTemplate(
+            'TestimonialTextIsRequired',
+            __('Testimonial Text cannot be blank.')
+        );
+    }
+}

app/code/Training/Testimonials/Model/Ui/DataProvider.php
@@ -27,7 +27,6 @@ class DataProvider extends AbstractDataProvider
 
         $items = $this->collection->getItems();
         $this->loadedData = array();
-
         foreach ($items as $testimonial) {
             $this->loadedData[$testimonial->getId()]['testimonial'] = $testimonial->getData();
         }

commit e1c99e41c8b7ac8013e72f6151c9b3b8693cf049

Fix massDelete Bug
Fixes: d130a2e7ae84b382615bd54dfe54363353c1b542

app/code/Training/Testimonials/Controller/Adminhtml/Testimonial/MassDelete.php
@@ -17,7 +17,8 @@ class MassDelete extends Action
         Filter $filter,
         CollectionFactory $collectionFactory,
         RedirectFactory $resultRedirectFactory,
-        Action\Context $context)
+        Action\Context $context
+    )
     {
 
         $this->filter = $filter;
@@ -29,8 +30,8 @@ class MassDelete extends Action
     public function execute()
     {
         $collection =  $this->filter->getCollection($this->collectionFactory->create());
-        $size = $collection->getSize();
-
+        $size = $collection->count();
+        
         foreach ($collection as $item) {
             $item->delete();
         }

app/code/Training/Testimonials/Model/ResourceModel/Testimonial/Collection.php
@@ -8,6 +8,7 @@ use Training\Testimonials\Model\ResourceModel\Testimonial as TestimonialResource
 
 class Collection extends AbstractCollection
 {
+    protected $_idFieldName = 'id';
 
     protected function _construct()
     {

app/code/Training/Testimonials/etc/adminhtml/menu.xml
@@ -7,7 +7,7 @@
 -->
 <config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="urn:magento:module:Magento_Backend:etc/menu.xsd">
     <menu>
-        <add id="Training_Testimonials::testimonials" title="Testimonials" translate="title" module="Training_Testimonials" sortOrder="200" resource="Training_Testimonials::parent"/>
-        <add id="Training_Testimonials::testimonials_menu" title="Manage Testimonials" translate="title" module="Training_Testimonials" sortOrder="200" parent="Training_Testimonials::testimonials" action="testimonials/testimonial/index" resource="Training_Testimonials::parent"/>
+        <add id="Training_Testimonials::testimonials" title="Testimonials" translate="title" module="Training_Testimonials" sortOrder="70" resource="Training_Testimonials::parent"/>
+        <add id="Training_Testimonials::testimonials_menu" title="Manage Testimonials" translate="title" module="Training_Testimonials" sortOrder="70" parent="Training_Testimonials::testimonials" action="testimonials/testimonial/index" resource="Training_Testimonials::parent"/>
     </menu>
 </config>